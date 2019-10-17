eca72cc24c589aace206c9b4e192b1d279bd15c1
Trịnh Văn Duy <49477836+Duytv081298@users.noreply.github.com>
2019-10-17 22:15:27 +0700

    2222

---
+++ Btvn_Buoi2/app/src/main/java/com/android/btvn_buoi2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.btvn_buoi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai4);
+    }
+}
---
+++ btvn_buoi1/app/src/main/java/com/android/btvn_buoi1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.btvn_buoi1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai1);
+    }
+}
